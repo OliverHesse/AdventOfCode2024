feb7b5df224746daa3e424fd9c12ad14cd35fb3e
lunarLucent <109795653+lunarLucent@users.noreply.github.com>
2024-12-07 19:27:45 +0000

    added some more optimisations got it to around 10s

--- Day7/Part1then2Optimised.py
+++ Day7/Part1then2Optimised.py
@@ -11,94 +11,138 @@ def add(a,b):
 def concat(a,b):
     return int(f"{a}{b}")
 
+def sub(a,b):
+    value = a-b
+    if value <0:
+        return False
+    return value
+
+def unconcat(a,b):
+    a = str(a)
+    b = str(b)
+    b_len = len(b)
+    if len(a) <= b_len:
+        return False
+    value = a[0:len(a)-len(b)]
+    if a[len(a)-len(b):len(a)].replace(b,"") == "":
+        return int(value)
+    return False
+def div(a,b):
+    value = a//b
+    if a%b != 0:
+        return False
+    return value
 
 def main(file):
+    print("====starting====\n\n")
+    equations = []
+
+    with open(file+".txt","r") as input_file:
+        for line in input_file:
+            solution, operands = line.strip().split(":")
+            operands = operands.strip().split(" ")
+            operands = list(map(lambda x: int(x), operands))
+            equations.append([int(solution), operands])
+              
     confirmed_indexes = set()
     part1_start_time = time.process_time()
-    with open(file+".txt","r") as input_file:
+    #PART1
+    available_operations =[
+        mul,
+        add
+    ]
+    running_sum = 0
+    count = 0
+    
+    for equation in equations:
+        solution = equation[0]
+        components = equation[1]
+       
+        permutations = list(product(range(0, len(available_operations)), repeat=len(components)-1))
         
-        available_operations =[
-            mul,
-            add
-        ]
-        running_sum = 0
-        count = 0
-        for line in input_file:
-            solution,components = line.split(":")
-            components = components.strip().split(" ")
-            solution = int(solution)
-
-         
-            perm_time = time.process_time()
-            permutations = list(product(range(0, len(available_operations)), repeat=len(components)-1))
-            #print("generated permutations: "+ str(time.process_time()-perm_time))
+        
+        
+        for permutation in permutations:
             
-            for permutation in permutations:
-             
-                running_total = int(components[0])
-                early_break = False
-                for i in range(1,len(components)):
-                    running_total = available_operations[permutation[i-1]](running_total,int(components[i]))
-                    if running_total > solution:
-                        early_break = True
-                        break
-                if early_break:
-                    continue
-                if running_total == solution:
-                    running_sum += solution
-                    confirmed_indexes.add(count)
+            running_total = components[0]
+            early_break = False
+            for i in range(1,len(components)):
+                running_total = available_operations[permutation[i-1]](running_total,components[i])
+                if running_total > solution:
+                    early_break = True
                     break
-            count += 1 
-        print(running_sum)      
-    print("part1 finished in: "+str(time.process_time()-part1_start_time) +"s")
-    print(str(len(confirmed_indexes))+" confirmed index's")
-    part2_start_time = time.process_time()
-    with open(file+".txt","r") as input_file:
-        
-        available_operations =[
-            mul,
-            add,
-            concat
-        ]
-        running_sum = 0
-        count = 0
-        for line in input_file:
-            solution,components = line.split(":")
-      
-            solution = int(solution)
-            if count in confirmed_indexes:
-                
-                running_sum += solution
-                count += 1
+            if early_break:
                 continue
-            components = components.strip().split(" ")
+            if running_total == solution:
+                running_sum += solution
+                confirmed_indexes.add(count)
+                break
+        count += 1 
+    
+    print(running_sum)      
+    print("part1 finished in: "+str(time.process_time()-part1_start_time) +"s")
+    print("")
+    print("")
+    part2_start_time = time.process_time()
+    #Part2
+    existing_perms = {}
+    reverse_operators = [div,sub,unconcat]
+    
+    running_sum = 0
+    count = 0
+    for equation in equations:
+        solution = equation[0]
+        components = equation[1]
         
-
-         
+        
+       
+        
+      
+        #used to cheat a bit lol
+        #currently shaves off abt 2.2s
+        if count in confirmed_indexes:
             
-            permutations = list(product(range(0, len(available_operations)), repeat=len(components)-1))
-            #print("generated permutations: "+ str(time.process_time()-perm_time))
-            
-            for permutation in permutations:
-             
-                running_total = int(components[0])
-                early_break = False
-                for i in range(1,len(components)):
-                    running_total = available_operations[permutation[i-1]](running_total,int(components[i]))
-                    if running_total > solution:
-                        early_break = True
-                        break
-                if early_break:
-                    continue
-                if running_total == solution:
-                    running_sum += solution
-                    break
+            running_sum += solution
             count += 1
-        print(running_sum)      
-    print("part2 finished in: "+str(time.process_time() - part2_start_time)+"s")  
+            continue
+        
+        
+        permutations = []
+        if len(components)-1 in existing_perms:
+            permutations = existing_perms[len(components)-1]
+        else:
+            permutations = list(product(range(0, len(reverse_operators)), repeat=len(components)-1))
+            existing_perms[len(components)-1] = permutations
+
+        for permutation in permutations:
+            
+            #backwards
+            running_total = solution
+            early_break = False
+            for i in range(len(permutation)-1,-1,-1):
+                
+                new_val = reverse_operators[permutation[i]](running_total,components[i+1])
+            
+                if new_val == False:
+                    
+                    early_break = True
+                    break
+                running_total = new_val
+            if early_break:
+                continue
+            
+            if running_total == components[0]:
+                #print("found on line: "+str(count)+" with permutation " +str(permutation))
+                
+                running_sum+=solution
+                break
+        
+        count += 1
+    print(running_sum)      
+    print("part2 finished in: "+str(time.process_time() - part2_start_time)+"s\n\n")  
 
 start_time = time.process_time()
 #main("test_input")
 main("input")
-print(time.process_time() - start_time, "seconds") 
+print("Total time taken: "+ str(time.process_time() - start_time), "s") 
 
